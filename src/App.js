2eb9d86331d94d7c8d2664a942d209b1d9a7d4a0
zaawmilen <166408897+zaawmilen@users.noreply.github.com>
2024-05-24 09:56:23 +0300

    banner navbar row

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
+import React from 'react';
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Row from './components/Row';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> NETFLIX</h1>
+    <div className="app">
+      <Navbar />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl="/netflix-originals" isLargeRow />
+      <Row title="Trending Now" fetchUrl="/trending" />
+      <Row title="Top Rated" fetchUrl="/top-rated" />
+      <Row title="Action Movies" fetchUrl="/action" />
+      <Row title="Comedy Movies" fetchUrl="/comedy" />
+      <Row title="Horror Movies" fetchUrl="/horror" />
+      <Row title="Romance Movies" fetchUrl="/romance" />
+      <Row title="Documentaries" fetchUrl="/documentaries" />
     </div>
   );
 }
